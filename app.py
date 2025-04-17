ba5059536504339cad1fce1950f7dc497cbe263d
mounikao <mounika365@gmail.com>
2025-04-17 23:46:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello, Dockerized World!"
+REQUEST_COUNT = Counter('request_count', 'Total web requests')
 
-if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+@app.route("/")
+def home():
+    REQUEST_COUNT.inc()
+    return "Hello from Jenkins to K8s!"
+
+@app.route('/metrics')
+def metrics():
+    return generate_latest(), 200, {'Content-Type': CONTENT_TYPE_LATEST}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
